26eb85bfdccf1c626b9ba13e16a14470245fbe0e
Razacx <levi.vandenbempt.sjcm@gmail.com>
2016-11-18 23:21:47 +0100

    Test UI launch

--- src/launcher/Launcher.java
+++ src/launcher/Launcher.java
@@ -1,4 +1,19 @@
 package launcher;
 
+import view.GameView;
+import view.controller.Controller;
+
 public class Launcher {
+
+    public static void main(String[] args) {
+
+        new GameView(new Controller() {
+            @Override
+            public int hashCode() {
+                return super.hashCode();
+            }
+        }).start();
+
+    }
+
 }
