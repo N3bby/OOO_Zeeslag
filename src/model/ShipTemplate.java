2e30d34a8478685fb4ecefe71114d81fd3d06184
numensis <sander.willems@student.ucll.be>
2016-11-19 18:15:10 +0100

    ShipTemplate klasse gemaakt

---
+++ src/model/ShipTemplate.java
@@ -0,0 +1,26 @@
+package model;
+
+/**
+ * 
+ * @author Sander Willems
+ *
+ */
+
+public enum ShipTemplate {
+	AIRCRAFT_CARRIER(5),
+	BATTLESHIP(4),
+	SUBMARINE(3),
+	DESTROYER(3),
+	PATROL_BOAT(2);
+	
+	private int nbrOfCells;
+	
+	private ShipTemplate(int nbrOfCells){
+		this.nbrOfCells = nbrOfCells;
+	}
+	
+	public int getNbrOfCells(){
+		return this.nbrOfCells;
+	}
+
+}
