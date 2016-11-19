28ef341b271f73834c5666c0cede0544fc23bb2b
numensis <sander.willems@student.ucll.be>
2016-11-19 21:37:21 +0100

    Strings die worden meegegeven van ShipFactory aan Ship constructor gewijzigd naar ander format, e.g. "Aircraft Carrier"

--- src/model/ShipFactory.java
+++ src/model/ShipFactory.java
@@ -11,11 +11,36 @@ public class ShipFactory {
 		
 		String name = type.name();
 		
+		name = name.toLowerCase();
+		name = name.replace('_', ' ');
+		name = this.capitalize(name);
+		
+		
 		Ship ship = new Ship(name, x, y, orientation);
 		
 		return ship;
 		
 		
+	}
+	
+	
+	private String capitalize(String str){
+		
+		//Zet de eerste letter achter de spatie in hoofdletter (voor de tweedelige namen)
+		if(str.contains(" ")){
+			for (int i=0; i<str.length(); i++){
+				if(str.charAt(i) == ' '){
+					char chr = str.charAt(i+1);
+					chr = Character.toUpperCase(chr);
+					str = str.substring(0, i+1) + chr + str.substring(i+2);
+					break;
+				}
+					
+			}
+		}
+		
+		//Zet de eerste letter in hoofdletter
+		return Character.toUpperCase(str.charAt(0)) + str.substring(1);
 	}
 
 }
