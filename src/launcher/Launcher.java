916b1725a5f2a9383da4be20c1e6531fc30c3f6f
Razacx <levi.vandenbempt.sjcm@gmail.com>
2016-11-21 00:19:24 +0100

    Fixed launcher

--- src/launcher/Launcher.java
+++ src/launcher/Launcher.java
@@ -10,9 +10,9 @@ public class Launcher {
     public static void main(String[] args) {
 
         GameView gameView = new GameView();
-        Game game = new Game();
-        ModelFacade modelFacade = new ModelFacade(game);
+        ModelFacade modelFacade = new ModelFacade();
         Controller controller = new Controller(gameView, modelFacade);
+        gameView.setController(controller);
         controller.startView();
 
     }
