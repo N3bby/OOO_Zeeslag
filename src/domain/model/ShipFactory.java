f4d02745be83248587c00875c90084848ffa3201
Razacx <levi.vandenbempt.sjcm@gmail.com>
2016-11-21 00:19:05 +0100

    Moved method for capitalized names from ShipFactory to ShipTemplate

--- src/domain/model/ShipFactory.java
+++ src/domain/model/ShipFactory.java
@@ -1,52 +1,18 @@
 package domain.model;
 
-/**
- * 
- * @author Sander Willems
- *
- */
-
-
 public class ShipFactory {
-	
-	public ShipFactory(){
-	}
-	
-	public Ship createShipFromTemplate(ShipTemplate type, int x, int y ,Orientation orientation){
-		
-		String name = type.name();
-		int length = type.getNbrOfCells();
-		
-		name = name.toLowerCase();
-		name = name.replace('_', ' ');
-		name = this.capitalize(name);
-		
-		
-		Ship ship = new Ship(name, length, x, y, orientation);
-		
-		return ship;
-		
-		
-	}
-	
-	
-	private String capitalize(String str){
-		
-		//Zet de eerste letter achter de spatie in hoofdletter (voor de tweedelige namen)
-		if(str.contains(" ")){
-			for (int i=0; i<str.length(); i++){
-				if(str.charAt(i) == ' '){
-					char chr = str.charAt(i+1);
-					chr = Character.toUpperCase(chr);
-					str = str.substring(0, i+1) + chr + str.substring(i+2);
-					break;
-				}
-					
-			}
-		}
-		
-		//Zet de eerste letter in hoofdletter
-		return Character.toUpperCase(str.charAt(0)) + str.substring(1);
-	}
+
+    public ShipFactory() {
+
+    }
+
+    public Ship createShipFromTemplate(ShipTemplate type, int x, int y, Orientation orientation) {
+
+        String name = ShipTemplate.formattedName(type);
+        int length = type.getNbrOfCells();
+
+        return new Ship(name, length, x, y, orientation);
+
+    }
 
 }
--- src/domain/model/ShipTemplate.java
+++ src/domain/model/ShipTemplate.java
@@ -1,12 +1,7 @@
 package domain.model;
 
-/**
- * 
- * @author Sander Willems
- *
- */
-
 public enum ShipTemplate {
+
 	AIRCRAFT_CARRIER(5),
 	BATTLESHIP(4),
 	SUBMARINE(3),
@@ -21,6 +16,28 @@ public enum ShipTemplate {
 	
 	public int getNbrOfCells(){
 		return this.nbrOfCells;
+	}
+
+	public static String formattedName(ShipTemplate shipTemplate){
+
+		String str = shipTemplate.toString().toLowerCase().replace("_", " ");
+
+		//Zet de eerste letter achter de spatie in hoofdletter (voor de tweedelige namen)
+		if(str.contains(" ")){
+			for (int i=0; i<str.length(); i++){
+				if(str.charAt(i) == ' '){
+					char chr = str.charAt(i+1);
+					chr = Character.toUpperCase(chr);
+					str = str.substring(0, i+1) + chr + str.substring(i+2);
+					break;
+				}
+
+			}
+		}
+
+		//Zet de eerste letter in hoofdletter
+		return Character.toUpperCase(str.charAt(0)) + str.substring(1);
+
 	}
 
 }
