dd50f552bf0654e89352abbaa191c131ccd5c31f
Razacx <levi.vandenbempt.sjcm@gmail.com>
2016-11-21 15:40:35 +0100

    Checking logic

--- src/domain/model/Board.java
+++ src/domain/model/Board.java
@@ -16,6 +16,8 @@ public class Board implements BoardObservable {
 
 	private List<BoardObserver> boardObservers = new ArrayList<>();
 
+    private List<Ship> placedShips = new ArrayList<>();
+
     public Board(Player player) {
         this.player = player;
         cells = new CellState[WIDTH_HEIGHT][WIDTH_HEIGHT];
@@ -31,7 +33,12 @@ public class Board implements BoardObservable {
 	}
 	
 	public void applyShip(Ship ship) {
-        System.out.println(ship);
+
+        if(placedShips.size() >= 5) throw new RuntimeException("Can only place 5 ships");
+        if(!checkShipCountValid(ship)) throw new RuntimeException("Can't place any more of this ship");
+        if(!checkShipPositionValid(ship)) throw new RuntimeException("Invalid placement of ship");
+        if(!checkShipDoesNotOverlap(ship)) throw new RuntimeException("Ship overlaps");
+
         if(ship.getOrientation() == Orientation.VERTICAL) {
 			for(int i = ship.getY(); i < ship.getY() + ship.getLength() && i < WIDTH_HEIGHT; i++) {
                 cells[i][ship.getX()] = new ShipCellState();
@@ -42,8 +49,75 @@ public class Board implements BoardObservable {
                 cells[ship.getY()][i] = new ShipCellState();
             }
 		}
+		placedShips.add(ship);
 		notifyBoardChanged();
+
 	}
+
+    private boolean checkShipDoesNotOverlap(Ship ship) {
+
+        int xMin = ship.getX() - 1;
+        int xMax = -1;
+        int yMin = ship.getY() - 1;
+        int yMax = -1;
+
+        if(ship.getOrientation() == Orientation.HORIZONTAL) {
+
+            xMax = ship.getX() + ship.getLength();
+            yMax = ship.getY() + 1;
+
+        } else if(ship.getOrientation() == Orientation.VERTICAL) {
+
+            xMax = ship.getX() + 1;
+            yMax = ship.getY() + ship.getLength();
+
+        }
+
+        if(xMin < 0) xMin = 0;
+        if(yMin < 0) yMin = 0;
+        if(xMax > 9) xMax = 9;
+        if(yMax > 9) yMax = 9;
+
+        for (int x = xMin; x <= xMax; x++) {
+            for (int y = yMin; y <= yMax; y++) {
+                if(!(cells[y][x] instanceof EmptyCellState)) return false;
+            }
+        }
+
+        return true;
+
+    }
+
+    private boolean checkShipPositionValid(Ship ship) {
+
+        if(ship.getOrientation() == Orientation.HORIZONTAL) {
+
+            if(ship.getX() < 0 || ship.getX() > 9) return false;
+            if(ship.getY() < 0 || ship.getY() > 9) return false;
+            if(ship.getX() + ship.getLength() < 0 || ship.getX() + ship.getLength() > 10) return false;
+
+        } else if (ship.getOrientation() == Orientation.VERTICAL) {
+
+            if(ship.getX() < 0 || ship.getX() > 9) return false;
+            if(ship.getY() < 0 || ship.getY() > 9) return false;
+            if(ship.getY() + ship.getLength() < 0 || ship.getY() + ship.getLength() > 10) return false;
+
+        }
+
+        return true;
+
+    }
+
+    private boolean checkShipCountValid(Ship ship) {
+
+        if(placedShips.size() >= 5) return false;
+
+        ArrayList<Ship> newPlacedShips = new ArrayList<>(placedShips);
+        newPlacedShips.add(ship);
+
+        return !ShipTemplate.containsShipsOverMaxCount(newPlacedShips);
+
+    }
 
     public Player getPlayer() {
         return player;
--- src/domain/model/ShipTemplate.java
+++ src/domain/model/ShipTemplate.java
@@ -1,26 +1,49 @@
 package domain.model;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public enum ShipTemplate {
 
-	AIRCRAFT_CARRIER(5),
-	BATTLESHIP(4),
-	SUBMARINE(3),
-	DESTROYER(3),
-	PATROL_BOAT(2);
+	AIRCRAFT_CARRIER(5, 1),
+	BATTLESHIP(4, 2),
+	SUBMARINE(3, 3),
+	DESTROYER(3, 3),
+	PATROL_BOAT(2, 4);
 	
 	private int nbrOfCells;
-	
-	ShipTemplate(int nbrOfCells){
+    private int maxShipCount;
+
+    ShipTemplate(int nbrOfCells, int maxShipCount){
 		this.nbrOfCells = nbrOfCells;
-	}
-	
-	public int getNbrOfCells(){
+        this.maxShipCount = maxShipCount;
+    }
+
+    public static boolean containsShipsOverMaxCount(List<Ship> ships) {
+
+        for (ShipTemplate template : values()) {
+            //Grabs all ships of current template and puts them into a list
+            List<Ship> shipsOfTemplate = ships.stream()
+                    .filter(ship -> ship.getShipTemplate() == template)
+                    .collect(Collectors.toList());
+            if(shipsOfTemplate.size() > template.getMaxShipCount()) return true;
+        }
+
+        return false;
+
+    }
+
+    public int getMaxShipCount() {
+        return maxShipCount;
+    }
+
+    public int getNbrOfCells(){
 		return this.nbrOfCells;
 	}
 
-	public static String formattedName(ShipTemplate shipTemplate){
+	public String formattedName(){
 
-		String str = shipTemplate.toString().toLowerCase().replace("_", " ");
+		String str = this.toString().toLowerCase().replace("_", " ");
 
 		//Zet de eerste letter achter de spatie in hoofdletter (voor de tweedelige namen)
 		if(str.contains(" ")){
