a11203383d7f530a0252c61d65ac9bdefd1b2a8e
Razacx <levi.vandenbempt.sjcm@gmail.com>
2016-11-28 15:16:48 +0100

    Added gamestate checks for certain actions in Game

--- src/domain/model/Game.java
+++ src/domain/model/Game.java
@@ -1,22 +1,26 @@
 package domain.model;
 
 import domain.model.state.game.GameState;
+import domain.model.state.game.NamingGameState;
 
 import java.util.Arrays;
 
 public class Game {
 
-	private GameState gameState;
+    private GameState gameState;
     private Player[] players = new Player[2];
     private int currentTurn = 0;
 
     public void addPlayer(String name, boolean isAi) {
+        if (!(gameState instanceof NamingGameState))
+            throw new IllegalStateException("Cannot add players while not in the NamingGameState");
         Player player = new Player(name);
-        if(isAi) player = new AiPlayer(name);
+        if (isAi) player = new AiPlayer(name);
         if (players[0] == null) {
             players[0] = player;
         } else if (players[1] == null) {
             players[1] = player;
+            gameState.proceed(this); //Go to the next state (Ship placement)
         } else {
             throw new IndexOutOfBoundsException("There are aleady 2 players");
         }
@@ -24,8 +28,8 @@ public class Game {
 
     public Player getPlayer(String name) {
         for (Player player : players) {
-            if(player != null) {
-                if(player.getName().equals(name)) return player;
+            if (player != null) {
+                if (player.getName().equals(name)) return player;
             }
         }
         throw new RuntimeException("No player with name '" + name + "' found!");
@@ -36,8 +40,8 @@ public class Game {
     }
 
     public void nextTurn() {
-        if(currentTurn == 0) currentTurn = 1;
-        if(currentTurn == 1) currentTurn = 0;
+        if (currentTurn == 0) currentTurn = 1;
+        if (currentTurn == 1) currentTurn = 0;
         Arrays.stream(players).forEach(p -> p.getBoard().notifyBoardChanged());
     }
 
