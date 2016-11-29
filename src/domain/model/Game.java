83a8971d99b51e58cfbab9434e702beb7d9667a4
Razacx <levi.vandenbempt.sjcm@gmail.com>
2016-11-29 18:54:20 +0100

    Game is now a GameStateObservable and TurnObservable. Also fixed a small bug

--- src/domain/model/Game.java
+++ src/domain/model/Game.java
@@ -1,26 +1,36 @@
 package domain.model;
 
+import domain.model.observable.gamestate.GameStateObservable;
+import domain.model.observable.gamestate.GameStateObserver;
+import domain.model.observable.turn.TurnObservable;
+import domain.model.observable.turn.TurnObserver;
 import domain.model.state.game.GameState;
 import domain.model.state.game.NewGameState;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
-public class Game {
+public class Game implements GameStateObservable, TurnObservable {
 
 	private GameState gameState = new NewGameState();
     private Player[] players = new Player[2];
     private int currentTurn = 0;
+    private List<GameStateObserver> gameStateObservers = new ArrayList<>();
+    private List<TurnObserver> turnObservers = new ArrayList<>();
 
     public void addPlayer(String name, boolean isAi) {
         if (!(gameState instanceof NewGameState))
             throw new IllegalStateException("Cannot add players while not in the NamingGameState");
         Player player = new Player(name);
-        if(isAi) player = new AiPlayer(name);
+        if(isAi) {
+            player = new AiPlayer(name);
+            addTurnObserver((AiPlayer)player);
+        }
         if (players[0] == null) {
             players[0] = player;
         } else if (players[1] == null) {
             players[1] = player;
-            gameState.proceed(this); //Go to the next state (Ship placement)
         } else {
             throw new IndexOutOfBoundsException("There are aleady 2 players");
         }
@@ -51,10 +61,47 @@ public class Game {
         	currentTurn = 0;
         }
         Arrays.stream(players).forEach(p -> p.getBoard().notifyBoardChanged());
+        notifyTurnChanged();
     }
     
     public GameState getGameState() {
     	return gameState;
+    }
+
+    public void setGameState(GameState gameState) {
+        this.gameState = gameState;
+        notifyGameStateChanged();
+    }
+
+    @Override
+    public void addGameStateObserver(GameStateObserver gameStateObserver) {
+        gameStateObservers.add(gameStateObserver);
+        if(gameStateObservers.size() == 1) notifyGameStateChanged(); //Do this because the NewGameState event won't be caught otherwise
+    }
+
+    @Override
+    public void removeGameStateObsserver(GameStateObserver gameStateObserver) {
+        gameStateObservers.remove(gameStateObserver);
+    }
+
+    @Override
+    public void notifyGameStateChanged() {
+        gameStateObservers.forEach(o -> o.gameStateChanged(gameState));
+    }
+
+    @Override
+    public void addTurnObserver(TurnObserver turnObserver) {
+        turnObservers.add(turnObserver);
+    }
+
+    @Override
+    public void removeTurnObserver(TurnObserver turnObserver) {
+        turnObservers.remove(turnObserver);
+    }
+
+    @Override
+    public void notifyTurnChanged() {
+        turnObservers.forEach(o -> o.turnChanged(this));
     }
 
 }
